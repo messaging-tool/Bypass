3324707c5d2dded808296224ea4c5313c89ce2d3
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-03-24 12:42:24 +0530

    Update urls.py

--- autho/urls.py
+++ autho/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('autho/', views.index, name='index'),
-    path('autho/twitter_login/', views.twitter_login, name='twitter_login'),
-    path('autho/twitter_callback/', views.twitter_callback, name='twitter_callback'),
-    path('autho/twitter_logout/', views.twitter_logout, name='twitter_logout'),
+    path('', views.index, name='index'),
+    path('twitter_login/', views.twitter_login, name='twitter_login'),
+    path('twitter_callback/', views.twitter_callback, name='twitter_callback'),
+    path('twitter_logout/', views.twitter_logout, name='twitter_logout'),
 
 ]
