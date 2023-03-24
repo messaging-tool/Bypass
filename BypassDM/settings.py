b9207f0eca78b4e99499cf09fdbb2f0c679654eb
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-03-25 05:11:06 +0530

    Update settings.py

--- BypassDM/settings.py
+++ BypassDM/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -7,7 +8,9 @@ from decouple import config
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+#SECRET_KEY = config('DJANGO_SECRET_KEY')
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool)
