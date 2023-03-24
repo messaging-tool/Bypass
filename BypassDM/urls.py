36da572e1d2343b6b3420bac402de511f41ecf68
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-03-25 01:49:50 +0530

    Update urls.py

--- BypassDM/urls.py
+++ BypassDM/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('LandingPage.urls')),
     path('autho/', include('autho.urls')),
     path('BypassDM_V1/', include('BypassDM_V1.urls'), name='BypassDM_V1'),
-    path('admin/', admin.site.urls),
 ]
