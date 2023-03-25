c7833f0a57b6ced5fe5d1f605b86b65f95428784
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-03-25 06:02:39 +0530

    Update settings.py

--- BypassDM/settings.py
+++ BypassDM/settings.py
@@ -8,9 +8,9 @@ from decouple import config
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+#SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool)
