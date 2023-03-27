0a156dea88a551b48abfc6640dfc3129f6350cfa
Adam Olanrewaju <68395177+Dharmzeey@users.noreply.github.com>
2023-03-28 00:20:06 +0100

    Update models.py

--- BypassDM_V1/models.py
+++ BypassDM_V1/models.py
@@ -8,7 +8,7 @@ class Tweet(models.Model):
     username = models.CharField(max_length=255)
     message = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    #uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     twitter_user_ids = models.CharField(max_length=100, default='')
     key = models.BinaryField(null=True)
