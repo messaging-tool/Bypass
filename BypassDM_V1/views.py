8c13c556f16e23aadcd9f2198e535b05324146b9
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-03-31 06:40:11 +0530

    Update views.py

--- BypassDM_V1/views.py
+++ BypassDM_V1/views.py
@@ -1,21 +1,25 @@
-import base64
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import Tweet, EncryptedMessage
+from autho.models import TwitterUser
+import tweepy
 from django.urls import reverse
 from django.conf import settings
 from tweepy import OAuthHandler, API
-from cryptography.fernet import Fernet
 from .forms import TweetForm
-from .models import Tweet, EncryptedMessage
-from autho.models import TwitterUser
+from autho.models import TwitterAuthToken, TwitterUser
 import uuid
+from cryptography.fernet import Fernet
+from django.views.decorators.csrf import csrf_protect
+
 
 
 SECRET_KEY = b'<045349f35b3d41e9b778798f1d4b561caf8084f4ba2640c3d9742ec6e960f6b6a7c3dc277ce4c1432cf0ad5ad1a8c3d5f99736afcafbf596ae6ea060348ceb55b13e869d6d>'
 
 
 @login_required
+@csrf_protect
 def tweet_view(request):
     try:
         twitter_user = TwitterUser.objects.get(user=request.user)
@@ -28,10 +32,7 @@ def tweet_view(request):
         if form.is_valid():
             # Get the Twitter user's access token and secret from the database
             auth = OAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
-            auth.set_access_token(
-                twitter_user.twitter_oauth_token.oauth_token,
-                twitter_user.twitter_oauth_token.oauth_token_secret
-            )
+            auth.set_access_token(twitter_user.twitter_oauth_token.oauth_token, twitter_user.twitter_oauth_token.oauth_token_secret)
             api = API(auth)
 
             # Get the username and message from the form
@@ -45,25 +46,11 @@ def tweet_view(request):
 
             # Construct the link with a unique UUID
             tweet_uuid = uuid.uuid4()
-            link = reverse('message', args=[tweet_uuid])
-
-            # ENCRYPT THE KEY
-            key_byte = base64.urlsafe_b64encode(key)
-            key_string = key_byte.decode()
+            link = f'https://BypassDM.com/BypassDM_V1/private_message/{tweet_uuid}/'
 
             # Save the tweet to the database
-            encrypted_message_obj = EncryptedMessage.objects.create(
-                encrypted_message=encrypted_message,
-                encrypted_text=encrypted_message
-            )
-            tweet = Tweet.objects.create(
-                twitter_user=twitter_user,
-                username=username,
-                key=key_string,
-                link=link,
-                twitter_userid=twitter_user.id,
-                message=encrypted_message_obj
-            )
+            encrypted_message_obj = EncryptedMessage.objects.create(encrypted_message=encrypted_message, encrypted_text=encrypted_message)
+            tweet = Tweet.objects.create(twitter_user=twitter_user, username=username, key=key, link=link, twitter_userid=twitter_user.id, message=encrypted_message_obj)
 
             # Construct the tweet message
             tweet_text = f'hello @{username}! I have a message for you: {link}'
@@ -78,143 +65,27 @@ def tweet_view(request):
     return render(request, 'BypassDM_V1/tweet.html', {'form': form})
 
 
-
+    
 @login_required
 def message_view(request, tweet_uuid):
-    try:
-        tweet = Tweet.objects.get(link__iexact=f'https://bypassdm.com/BypassDM_V1/private_message/{tweet_uuid}/')
-    except Tweet.DoesNotExist:
-        return render(request, 'BypassDM_V1/error.html', {'error_message': 'Invalid message link'})
-    
-    if tweet.username.lower() == request.user.username.lower():
-        try:
+        # tweet = Tweet.objects.get(link=f'https://bypassdm.com/private_message/{tweet_uuid}/')
+        
+        link_query = f'https://bypassdm.com/BypassDM_V1/private_message/{tweet_uuid}/'
+        tweet = Tweet.objects.get(link__iexact=link_query)
+        
+        
+        if tweet.username.lower() == request.user.username.lower():
             # Decrypt the message using the Fernet module and the secret key
-            key_string = tweet.key.encode()
-            key_byte = base64.urlsafe_b64decode(key_string + b'=' * (-len(key_string) % 4)) # padding the key with "="
-            f = Fernet(key_byte)
-            decrypted_message = f.decrypt(tweet.message.encrypted_message.encode()).decode()
-            
+            f = Fernet(tweet.key)
+            decrypted_message = f.decrypt(tweet.message.encrypted_text).decode()
+
             # Pass the decrypted message to the template
             return render(request, 'BypassDM_V1/message.html', {'message': decrypted_message})
-        except Exception as e:
-            return render(request, 'BypassDM_V1/error.html', {'error_message': 'Error decrypting the message'})
-    else:
         return render(request, 'BypassDM_V1/error.html', {'error_message': 'You are not authorized to view this message'})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import base64
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.decorators import login_required
-# from django.contrib import messages
-# from .models import Tweet, EncryptedMessage
-# from autho.models import TwitterUser
-# from django.urls import reverse
-# from django.conf import settings
-# from tweepy import OAuthHandler, API
-# from .forms import TweetForm
-# from autho.models import TwitterAuthToken, TwitterUser
-# import uuid
-# from cryptography.fernet import Fernet
-# from django.views.decorators.csrf import csrf_protect
-
-
-
-# SECRET_KEY = b'<045349f35b3d41e9b778798f1d4b561caf8084f4ba2640c3d9742ec6e960f6b6a7c3dc277ce4c1432cf0ad5ad1a8c3d5f99736afcafbf596ae6ea060348ceb55b13e869d6d>'
-
-
-# @login_required
-# @csrf_protect
-# def tweet_view(request):
-#     try:
-#         twitter_user = TwitterUser.objects.get(user=request.user)
-#     except TwitterUser.DoesNotExist:
-#         messages.error(request, 'Twitter user does not exist')
-#         return redirect('home')
-
-#     if request.method == 'POST':
-#         form = TweetForm(request.POST)
-#         if form.is_valid():
-#             # Get the Twitter user's access token and secret from the database
-#             auth = OAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
-#             auth.set_access_token(twitter_user.twitter_oauth_token.oauth_token, twitter_user.twitter_oauth_token.oauth_token_secret)
-#             api = API(auth)
-
-#             # Get the username and message from the form
-#             username = form.cleaned_data['username']
-#             message = form.cleaned_data['message']
-
-#             # Encrypt the message using a randomly generated key
-#             key = Fernet.generate_key()
-#             f = Fernet(key)
-#             encrypted_message = f.encrypt(message.encode())
-
-#             # Construct the link with a unique UUID
-#             tweet_uuid = uuid.uuid4()
-#             link = f'https://BypassDM.com/BypassDM_V1/private_message/{tweet_uuid}/'
-            
-#             # ENCRYPT THE KEY
-#             key_byte = base64.urlsafe_b64encode(key)
-#             key_string = key_byte.decode()
-
-#             # Save the tweet to the database
-#             encrypted_message_obj = EncryptedMessage.objects.create(encrypted_message=encrypted_message, encrypted_text=encrypted_message)
-#             tweet = Tweet.objects.create(twitter_user=twitter_user, username=username, key=key_string, link=link, twitter_userid=twitter_user.id, message=encrypted_message_obj)
-
-#             # Construct the tweet message
-#             tweet_text = f'hello @{username}! I have a message for you: {link}'
-
-#             # Construct the Twitter URL with pre-populated tweet
-#             twitter_url = f'https://twitter.com/intent/tweet?text={tweet_text}'
-
-#             # Redirect to Twitter for tweeting
-#             return redirect(twitter_url)
-#     else:
-#         form = TweetForm()
-#     return render(request, 'BypassDM_V1/tweet.html', {'form': form})
-
-
-  
-
-    
-# @login_required
-# def message_view(request, tweet_uuid):
-#         # tweet = Tweet.objects.get(link=f'https://bypassdm.com/private_message/{tweet_uuid}/')
-        
-#         link_query = f'https://bypassdm.com/BypassDM_V1/private_message/{tweet_uuid}/'
-#         tweet = Tweet.objects.get(link__iexact=link_query)
-        
-        
-#         if tweet.username.lower() == request.user.username.lower():
-#             # Decrypt the message using the Fernet module and the secret key
-#             # DECRYPT KEY
-#             key_string = tweet.key.encode()
-#             key_byte = base64.urlsafe_b64decode(key_string)
-#             f = Fernet(key_byte)
-#             decrypted_message = f.decrypt(tweet.message.encrypted_message).decode()
-
-#             # Decrypt the message using the Fernet module and the secret key
-#             f = Fernet(tweet.key)
-#             # decrypted_message = f.decrypt(tweet.message.encrypted_text).decode()
-
-#             # Pass the decrypted message to the template
-#             return render(request, 'BypassDM_V1/message.html', {'message': decrypted_message})
-#         return render(request, 'BypassDM_V1/error.html', {'error_message': 'You are not authorized to view this message'})
 
 
 
