f07b67d6db0229071371c89a13e978904823b984
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-04-02 12:45:02 +0530

    Update views.py

--- LandingPage/views.py
+++ LandingPage/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def landing_page(request):
-    return render(request, 'LandingPage/index.html')
+    return render(request, 'index.html')
 
