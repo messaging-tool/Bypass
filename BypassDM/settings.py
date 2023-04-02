690c916b82dff8fc1282a3a2986516ed09425403
Vinaygond <67134496+Vinaygond@users.noreply.github.com>
2023-04-02 10:19:57 +0530

    Update settings.py

--- BypassDM/settings.py
+++ BypassDM/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'LandingPage', 'templates', 'LandingPage', 'static'),
+    os.path.join(BASE_DIR, 'LandingPage', 'templates', 'LandingPage'),
 ]
 
 
