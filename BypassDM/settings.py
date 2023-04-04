74cc79af33d3c7e7a0ab5fbf98cf8a10825ff337
Vinay <67134496+Vinaygond@users.noreply.github.com>
2023-04-04 13:20:51 +0530

    Update settings.py

--- BypassDM/settings.py
+++ BypassDM/settings.py
@@ -141,15 +141,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'LandingPage', 'templates', 'static'),
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'LandingPage/templates/LandingPage/static']
+
 
 
 # Default primary key field type
