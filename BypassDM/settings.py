d2270a2bfb44c65539fa215d7c9081fdbe70926d
Vinay <67134496+Vinaygond@users.noreply.github.com>
2023-04-22 13:04:57 +0530

    Update settings.py

--- BypassDM/settings.py
+++ BypassDM/settings.py
@@ -160,6 +160,14 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'LandingPage/templates/LandingPage/static']
 
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
