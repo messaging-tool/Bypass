d86ba52c10546e3dc1b644b07846efd28426708c
Vinay <67134496+Vinaygond@users.noreply.github.com>
2023-04-23 14:47:39 +0530

    Update urls.py

--- LandingPage/urls.py
+++ LandingPage/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('terms-of-use/', views.landing_page, name='terms_of_use'),
+    path('privacy-policy/', views.landing_page, name='privacy_policy'),
 ]
