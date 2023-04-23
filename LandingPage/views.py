5e5a74f43830ad938e4f348cbcf94ebd834ba67c
Vinay <67134496+Vinaygond@users.noreply.github.com>
2023-04-23 14:46:58 +0530

    Update views.py

--- LandingPage/views.py
+++ LandingPage/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 def landing_page(request):
     return render(request, 'LandingPage/index.html')
 
+
+def privacy_policy(request):
+    return render(request, 'LandingPage/privacy_policy.html')
+
+
+def terms_of_use(request):
+    return render(request, 'LandingPage/terms_of_use.html')
