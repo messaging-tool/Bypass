6727bfe2f2326e6bb22664a1903efa52ef7fcc9c
Vinay <67134496+Vinaygond@users.noreply.github.com>
2023-06-11 14:44:21 +0530

    Update forms.py

--- BypassDM_V1/forms.py
+++ BypassDM_V1/forms.py
@@ -1,7 +1,15 @@
-from django import forms
+from tinymce.widgets import TinyMCE
 
 class TweetForm(forms.Form):
     username = forms.CharField(max_length=255)
-    message = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(widget=TinyMCE())
+
+
+
+# from django import forms
+
+# class TweetForm(forms.Form):
+#     username = forms.CharField(max_length=255)
+#     message = forms.CharField(widget=forms.Textarea)
 
 
